fc7f16eccf0efe5d1a60b0e7cfb01cfdd2e35546
Isaac Agyei <agyeis@hotmail.com>
2023-02-21 21:16:15 -0700

    change jwt expiry time

--- server/config/auth.config.js
+++ server/config/auth.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   secret: process.env.ACCESS_TOKEN_SECRET,
-  jwtExpiration: 600, //  10 minutes
-  jwtRefreshExpiration: 1200 // 20 minutes
+  jwtExpiration: 600, //  10 minutes 600
+  jwtRefreshExpiration: 1200,  // 20 minutes 1200
 }
