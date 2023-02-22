a249a62173a3a99867769de4ca4c8451e6ce4e1c
Isaac Agyei <agyeis@hotmail.com>
2023-02-21 19:08:18 -0700

    rmv card  elements

--- client/src/components/cards/invoices/InvoiceView.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Card from '@mui/material/Card';
-import Box from '@mui/material/Box'
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
